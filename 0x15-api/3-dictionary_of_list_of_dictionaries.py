28241fb8ada497ac1535b600b1661605b28adf4e
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-24 12:28:03 -0700

    Showing all todo tasks

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Fetching JSON data from API """
+
+import json
+import requests
+
+if __name__ == "__main__":
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    user_dict = requests.get(user_url).json()
+    file1_name = "todo_all_amployees.json"
+    the_dict = {}
+
+    for elem in user_dict:
+        name = elem.get("username")
+        user_id = str(elem.get("id"))
+        user_data = requests.get("{}/{}/todos".format(user_url, user_id))
+        user_data = user_data.json()
+        the_dict[user_id] = []
+        for item in user_data:
+            in_dict = {}
+            in_dict["username"] = name
+            in_dict["task"] = item.get("title")
+            in_dict["completed"] = item.get("completed")
+            the_dict[user_id].append(in_dict)
+
+    with open(file1_name, 'w') as f:
+        json.dump(the_dict, f)
